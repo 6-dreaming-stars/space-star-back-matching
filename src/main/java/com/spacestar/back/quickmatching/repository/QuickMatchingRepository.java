90aa7eb00d627df9bc710437735ee526995dd258
chojunho <chojunho0310@naver.com>
2024-06-10 13:49:56 +0900

    Feat : QuickMatching entity,repository

--- src/main/java/com/spacestar/back/quickmatching/repository/QuickMatchingRepository.java
+++ src/main/java/com/spacestar/back/quickmatching/repository/QuickMatchingRepository.java
@@ -1,10 +1,28 @@
 package com.spacestar.back.quickmatching.repository;
 
 import com.spacestar.back.quickmatching.domain.QuickMatching;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+@Repository
+public class QuickMatchingRepository {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private static final String KEY = "QuickMatching";
 
-public interface QuickMatchingRepository extends JpaRepository<QuickMatching,Long> {
-    List<QuickMatching> findByMatchFromMember(String matchFromMember);
+    public QuickMatchingRepository(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void save(QuickMatching quickMatching) {
+        System.out.println("quickMatching.getId() = " + quickMatching.getId());
+        redisTemplate.opsForZSet().add(KEY,  quickMatching,System.currentTimeMillis());
+    }
+
+    public QuickMatching findById(String id) {
+        return (QuickMatching) redisTemplate.opsForHash().get(KEY, id);
+    }
+
+    public void delete(String id) {
+        redisTemplate.opsForHash().delete(KEY, id);
+    }
 }
